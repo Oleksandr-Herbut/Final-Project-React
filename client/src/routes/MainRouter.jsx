7daf273ca49a7aa47834cf0e683b256cb4d2e0e4
Oleksandr-Herbut <berkyt892@gmail.com>
2024-09-18 20:16:33 +0200

    Add routes

--- client/src/routes/MainRouter.jsx
+++ client/src/routes/MainRouter.jsx
@@ -1,7 +1,14 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { Categories } from "../pages/Categories";
+import { Category } from "../pages/Category";
 
 export const MainRouter = () => {
-    return <Routes>
-        <Route path="/" element={<h1>Hi</h1>} />
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/categories/:categortyId" element={<Category />} />
     </Routes>
-}
+  );
+};
