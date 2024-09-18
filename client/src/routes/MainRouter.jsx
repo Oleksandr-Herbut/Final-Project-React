f0e01dab2bab9c6472ae225f5ff5fcf20f6175a3
Oleksandr-Herbut <berkyt892@gmail.com>
2024-09-18 21:21:18 +0200

    Добавлены роуты

--- client/src/routes/MainRouter.jsx
+++ client/src/routes/MainRouter.jsx
@@ -2,13 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Categories } from "../pages/Categories";
 import { Category } from "../pages/Category";
+import { Products } from "../pages/Products";
+import { Product } from "../pages/Product";
 
 export const MainRouter = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/categories" element={<Categories />} />
-      <Route path="/categories/:categortyId" element={<Category />} />
+      <Route path="/categories/:categoryId" element={<Category />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/products/:productId" element={<Product />} />
     </Routes>
   );
 };
