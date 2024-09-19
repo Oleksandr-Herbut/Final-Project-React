6f7242854817579d0841b1da736c40b7bdfb31f5
Oleksandr-Herbut <berkyt892@gmail.com>
2024-09-19 18:10:23 +0200

    Добавлен Footer

---
+++ client/src/components/Footer/FooterCard.jsx
@@ -0,0 +1,19 @@
+import { Box, Stack, Typography } from "@mui/material";
+
+export const FooterCard = ({ title, body, type }) => {
+  return (
+    <Box
+      sx={{
+        width: type === "lg" ? 780 : 548,
+        backgroundColor: "#F1F3F4",
+        padding: 4,
+        borderRadius: 3,
+      }}
+    >
+      <Stack direction="column">
+        <Typography mb={1}>{title}</Typography>
+        <Typography variant="h2">{body}</Typography>
+      </Stack>
+    </Box>
+  );
+};
