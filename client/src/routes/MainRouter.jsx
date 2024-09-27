d19dce022cc7afaa6e43b4d3bd5cf63268b4cfd7
Oleksandr-Herbut <berkyt892@gmail.com>
2024-09-27 18:03:58 +0200

    Добавлены роуты к соответствующим страницам

--- client/src/routes/MainRouter.jsx
+++ client/src/routes/MainRouter.jsx
@@ -1,11 +1,25 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Categories } from "../pages/Categories";
 import { Category } from "../pages/Category";
 import { Products } from "../pages/Products";
 import { Product } from "../pages/Product";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getCategories } from "../store/actionCreators";
+import { getProducts } from "../store/actionCreators";
+import { Sales } from "../pages/Sales";
+import { Cart } from "../pages/Cart";
+import { Error } from "../pages/Error";
 
 export const MainRouter = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+    dispatch(getProducts());
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
@@ -13,6 +27,11 @@ export const MainRouter = () => {
       <Route path="/categories/:categoryId" element={<Category />} />
       <Route path="/products" element={<Products />} />
       <Route path="/products/:productId" element={<Product />} />
+      <Route path="/sales" element={<Sales />} />
+      <Route path="/sales/:productId" element={<Product />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/error" element={<Error />} />
+      <Route path="*" element={<Navigate to="/404" />} />
     </Routes>
   );
 };
