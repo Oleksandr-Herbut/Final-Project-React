dd11e7a8dce4ed061a0df985d540ef779838a086
Oleksandr-Herbut <berkyt892@gmail.com>
2024-09-27 18:01:35 +0200

    Добавлен Category с товарами определённой категории и Сategories со всеми категориями

--- client/src/pages/Categories.jsx
+++ client/src/pages/Categories.jsx
@@ -1,3 +1,32 @@
+import { useSelector } from "react-redux";
+import { getCategories } from "../store/selectors";
+import { Layout } from "../layouts/Layout";
+import { CategoryCard } from "../components/CategoryCard";
+import { Typography, Grid2 } from "@mui/material";
+
 export const Categories = () => {
-    return <h1>Categories</h1>
-}
+  const categories = useSelector(getCategories);
+
+  return (
+    <Layout>
+      <Typography
+        style={{ fontWeight: "700", fontSize: "64px" }}
+        mb={5}
+        variant="h2"
+      >
+        Categories
+      </Typography>
+
+      <Grid2
+        mb={12}
+        container
+        rowSpacing={4}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+      >
+        {categories.map((category) => (
+          <CategoryCard category={category} key={category.id} />
+        ))}
+      </Grid2>
+    </Layout>
+  );
+};
--- client/src/pages/Category.jsx
+++ client/src/pages/Category.jsx
@@ -1,3 +1,62 @@
+import { Grid2, Typography } from "@mui/material";
+import { Layout } from "../layouts/Layout";
+import { useParams } from "react-router-dom";
+import { ProductCard } from "../components/ProductCard";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+import { filteredProducts, currentCategoryTitle } from "../utils";
+import { useSelector } from "react-redux";
+import { FilterComponent } from "../components/FilterComponent";
+
 export const Category = () => {
-  return <h1>Category</h1>;
+  // const [products, setProducts] = useState({});
+  const { categoryId } = useParams();
+  const categoryProducts = useSelector((state) =>
+    filteredProducts(state, Number(categoryId))
+  );
+  const category = useSelector((state) =>
+    currentCategoryTitle(state, Number(categoryId))
+  );
+
+  // useEffect(() => {
+  //   async function getProductsByCategory(id) {
+  //     try {
+  //       const response = await axios.get(
+  //         `http://localhost:3333/categories/${id}`
+  //       );
+  //       setProducts(response.data);
+  //     } catch (error) {
+  //       console.log(error.message);
+  //       return error.message;
+  //     }
+  //   }
+  //   getProductsByCategory(categoryId);
+  // }, [categoryId]);
+
+  return (
+    <Layout>
+      <div className="container">
+        <Typography mb={5} variant="h2">
+          {category}
+        </Typography>
+        <FilterComponent />
+        <Grid2
+          mb={12}
+          container
+          rowSpacing={4}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        >
+          {categoryProducts?.length > 0 ? (
+            categoryProducts?.map((product) => (
+              <Grid2 key={product?.id} size={3}>
+                <ProductCard product={product} />
+              </Grid2>
+            ))
+          ) : (
+            <Typography>Loading...</Typography>
+          )}
+        </Grid2>
+      </div>
+    </Layout>
+  );
 };
