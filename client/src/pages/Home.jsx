8c4ce256132f61e2809d4a0215e28aed6b01f010
Oleksandr-Herbut <berkyt892@gmail.com>
2024-09-27 17:29:07 +0200

    Добавлен страница Home которая выводит основные компоненты

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,32 @@
 import { Layout } from "../layouts/Layout";
+import { FirstBanner } from "../components/FirstBanner";
+import { useSelector } from "react-redux";
+import { selectRandomCategory } from "../store/selectors";
+import { CategoryBlock } from "../components/CategoryBlock";
+// import { getRandomProduct } from "../store/selectors";
+// import { CategoryCard } from "../components/CategoryCard";
+// import { ProductCard } from "../components/ProductCard";
+import FormDiscont from "../components/FormDiscont/FormDiscont";
+import { ProductBlock } from "../components/ProductBlock";
 
 export const Home = () => {
+  const randomCategories = useSelector(selectRandomCategory);
+
+  // const randomProducts = useSelector(getRandomProduct);
+
+  useSelector((state) => {
+    console.log(state);
+  });
+
   return (
     <Layout>
-      <div>
-        <h1>Home</h1>
-      </div>
+      <FirstBanner />
+      <CategoryBlock categories={randomCategories} />
+      <FormDiscont />
+      {/* {randomProducts.map((product) => (
+        <CategoryCard category={product} key={product.id} />
+      ))} */}
+      <ProductBlock />
     </Layout>
   );
 };
