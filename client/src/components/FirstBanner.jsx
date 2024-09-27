5847dece039d0ef87b20b46fa31e5dc96f3ec315
Oleksandr-Herbut <berkyt892@gmail.com>
2024-09-27 18:02:41 +0200

    Добавлен компонент FirstBanner

--- client/src/components/FirstBanner.jsx
+++ client/src/components/FirstBanner.jsx
@@ -1,14 +1,16 @@
 import { Box, Button, Typography } from "@mui/material";
 import banner from "../images/img.png";
+import { useNavigate } from "react-router-dom";
 
 export const FirstBanner = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <Box
         sx={{
           backgroundImage: `url(${banner})`,
           color: "#FFFFFF",
-          height: 700,
+          height: 600,
           padding: "80px 40px",
           mb: 10,
           backgroundRepeat: "no-repeat",
@@ -16,13 +18,17 @@ export const FirstBanner = () => {
         }}
       >
         <Typography
-          sx={{ maxWidth: 1200, fontWeight: "700" }}
+          sx={{ maxWidth: 1440, fontWeight: "700" }}
           mb={5}
           variant="h1"
         >
           Amazing Discounts on Pets Products!
         </Typography>
-        <Button variant="contained" sx={{ padding: "14px 56px" }}>
+        <Button
+          onClick={() => navigate("/sales")}
+          variant="contained"
+          sx={{ padding: "14px 56px" }}
+        >
           Check out
         </Button>
       </Box>
