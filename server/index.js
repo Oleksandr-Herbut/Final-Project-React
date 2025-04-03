09bd9197669131233d4334b4ea116e286620474d
Oleksandr Herbut <berkyt96@icloud.com>
2025-04-03 18:33:52 +0200

    Support for cors has been configured and some changes have been made

--- server/index.js
+++ server/index.js
@@ -1,48 +1,49 @@
-const { request } = require('express');
 const express = require('express');
 const categories = require('./routes/categories');
 const sale = require('./routes/sale');
 const order = require('./routes/order');
 const products = require('./routes/products');
 const sequelize = require('./database/database');
-const cors = require('cors')
+const cors = require('cors');
 const Category = require('./database/models/category');
 const Product = require('./database/models/product');
-const PORT = 3333;
+
+const PORT = process.env.PORT || 3333; // Используем process.env.PORT или 3333 по умолчанию
 
 Category.hasMany(Product);
 
 const app = express();
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+// Настраиваем CORS с переменной окружения CLIENT_ORIGIN
 app.use(cors({
-    origin: '*'
+  origin: process.env.CLIENT_ORIGIN || 'https://petshop-react-oleksandr.vercel.app/'
 }));
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true })); // Исправляем express.urlencoded()
+
 app.use('/categories', categories);
 app.use('/products', products);
 app.use('/sale', sale);
 app.use('/order', order);
 
-
-
-
 app.use(express.json());
 
-const start = async () =>{
-    try{
-        await sequelize.sync().then(
-            result => {/*console.log(result) */},
-            err => console.log(err)
-        );
-        
-        app.listen(PORT, ()=>{
-            console.log(`\n\nServer started on ${PORT} port...`)
-        })
-    }catch(err){
-        console.log(err);
-    }
-}
+const start = async () => {
+  try {
+    await sequelize.sync().then(
+      result => { /*console.log(result) */ },
+      err => console.log(err)
+    );
+
+    app.listen(PORT, () => {
+      console.log(`\n\nServer started on ${PORT} port...`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 start();
 
 // app.listen('3333');
